e8f8820b08c3cd9ebec1b9048f3ec0ce17f774b8
Yuri Shakhov <61325360+YuriShakhov@users.noreply.github.com>
2020-08-12 08:41:08 +0300

    Duplicated directory removed

--- app/src/main/java/com/example/worldnewsin/utils/NetworkUtils.java
+++
@@ -1,6 +0,0 @@
-package com.example.worldnewsin.utils;
-
-public class NetworkUtils {
-    public static final String YOUTUBE_API_BASE_URL = "https://www.googleapis.com/youtube/v3/";
-    public static final String API_KEY = null;
-}
